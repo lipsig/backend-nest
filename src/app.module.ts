ff2c05770de2229c6bf4c29acaece9acb3fa9df0
Felipe Lourenzi <felipesiegle@gmail.com>
2025-06-25 02:22:29 -0300

    feat: configure MongoDB in-memory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        const { MongoMemoryServer } = await import('mongodb-memory-server');
+        const mongod = await MongoMemoryServer.create();
+        const uri = mongod.getUri();
+        console.log('🚀 MongoDB em memória iniciado em:', uri);
+        return { uri };
+      }
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
