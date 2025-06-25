d6135519a8270297feef9086906d9ae704578d82
Felipe Lourenzi <felipesiegle@gmail.com>
2025-06-25 03:55:29 -0300

    chore(core): add dependencies and configure static files and global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,10 +10,17 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true, 
-      transform: true, 
+      transform: true,
+      validationError: {
+        target: false,
+        value: false,
+      },
     }),
   );
 
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   await app.listen(3000);
+  console.log('🚀 Aplicação rodando em http://localhost:3000');
 }
 bootstrap();
